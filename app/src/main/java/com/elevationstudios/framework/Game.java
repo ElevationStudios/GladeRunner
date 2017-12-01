505cb11989ef07863c5b95191407fd73688ac647
DPullarKhan <dpullarkhan@gmail.com>
2017-12-01 12:13:14 -0500

    Google Play Achievements
    In progress

--- app/src/main/java/com/elevationstudios/framework/Game.java
+++ app/src/main/java/com/elevationstudios/framework/Game.java
@@ -9,11 +9,11 @@ public interface Game
     public void setScreen(Screen screen);
     public Screen getCurrentScreen();
     public Screen getStartScreen();
-/*
+
     public boolean isSignedIn();
     public void signIn();
     public void submitScore(int score);
     public void showLeaderboards();
-    public void showAchievements();*/
+    public void showAchievements();
 }
 
