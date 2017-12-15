f5f11b3918383807e08872bad39ebe02979ce585
Koko Keheian <koko.keheian@Hotmail.com>
2017-12-14 20:26:11 -0500

    Background Scroll
    The background scrolls. There is some tearing after 3 instances of the image, which I have not been able to fix.

---
+++ app/src/main/java/com/elevationstudios/gladerunner/Background.java
@@ -0,0 +1,15 @@
+package com.elevationstudios.gladerunner;
+
+import com.elevationstudios.framework.Graphics;
+import com.elevationstudios.framework.Pixmap;
+
+public class Background {
+    public int xLocation;
+    public Pixmap background;
+
+    public Background(int xLoc, Pixmap map) {
+        this.xLocation  = xLoc;
+        this.background = map;
+    }
+
+}
