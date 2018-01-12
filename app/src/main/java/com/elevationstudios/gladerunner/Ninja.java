accfee2e416a572d02144763eceb8d1f9d8ab9fb
DPullarKhan <dpullarkhan@gmail.com>
2018-01-12 10:46:09 -0500

    Health bar fixed
    Health cannot go below 0

--- app/src/main/java/com/elevationstudios/gladerunner/Ninja.java
+++ app/src/main/java/com/elevationstudios/gladerunner/Ninja.java
@@ -63,6 +63,10 @@ public class Ninja {
     }
     public void setHealth(int num){
         health = num;
+        if (health < 0)
+            health = 0;
+        if (health > maxHealth)
+            health = maxHealth;
         CheckHealth();
     }
     public void takeDamage(int num){
